cab94a08fc4a270d28198711cb491e613dd7491e
Ben SpelledABC <benspelledabc@gmail.com>
2025-12-17 20:39:38 -0500

    fooork

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    host: true, // listen on all network interfaces
+    port: 5173,
+    allowedHosts: ['viking.spelledabc.org','ben.spelledabc.org', 'localhost', '127.0.0.1']
+  }
 });
