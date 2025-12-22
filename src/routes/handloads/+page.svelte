a81cccbecd8a86533a534746a1bcf290d6f1b56a
Ben SpelledABC <benspelledabc@gmail.com>
2025-12-21 22:07:24 -0500

    sorts .. for now

--- src/routes/handloads/+page.svelte
+++ src/routes/handloads/+page.svelte
@@ -17,6 +17,9 @@
       // const res = await fetch("http://viking.spelledabc.org:5000/api/v1/handloads");
       if (!res.ok) throw new Error(`Error ${res.status}: ${res.statusText}`);
       handloads = await res.json();
+      
+      handloads.sort((a, b) =>a.caliber.toLowerCase().localeCompare(b.caliber.toLowerCase()));
+
     } catch (err) {
       error = err.message;
     } finally {
