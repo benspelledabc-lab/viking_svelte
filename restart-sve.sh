2e73e8fdf9a52f3314a23c2e05da0bceddbf109b
Ben SpelledABC <BenSpelledABC@Gmail.com>
2026-01-03 10:54:34 +0000

    env for all

--- restart-sve.sh
+++ restart-sve.sh
@@ -1,2 +1,6 @@
+# put ENV files in place...
+aws s3 cp s3://spelledabc-asg-spot/toolbox/viking/env/svelte.env /workspace/svelte_ui/.env
+aws s3 cp s3://spelledabc-asg-spot/toolbox/viking/env/flask.env /workspace/flask_api/.env
+
 git pull
 docker-compose down && docker-compose up --build -d
